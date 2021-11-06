3d82c46fa3b75bc96642f623b01157631dc16b62
mostafa4adel <es-mostafaadel2023@alexu.edu.eg>
2021-11-07 00:25:32 +0200

    Timer and Max Speed
    Timer Code done and max speed value done

--- GUI/JSFiles/app.js
+++ GUI/JSFiles/app.js
@@ -12,9 +12,13 @@ const joystickLine = document.getElementById('joystickLine')
 const joystickCircle = document.getElementById('joystickCircle')
 
 const timer = document.getElementById('timer')
+const startTimer = document.getElementById('startTimer')
+const pauseTimer = document.getElementById('pauseTimer')
+const resetTimer = document.getElementById('resetTimer')
 
-const increaseSpeedBtn = document.getElementById('increaseSpeedBtn')
-const increaseSpeedSlider = document.getElementById('increaseSpeedSlider')
+const maxSpeedInput = document.getElementById('maxSpeedInput')
+maxSpeedInput.defaultValue = 50
+const increaseSpeedSlider = document.getElementById('range')
 
 const pidState = document.getElementById('pidState')
 const temprature = document.getElementById('temprature')
@@ -23,3 +27,56 @@ const velocity = document.getElementById('velocity')
 
 const dcArmState = document.getElementById('dcArmState')
 const pneumaticArmState = document.getElementById('pnumaticArmState')
+
+// variable to know if timer is running or not
+let runTimer = false
+let time = ''
+
+// ====================================Timer Code =============================
+
+startTimer.addEventListener('click', function () {
+  runTimer = true
+})
+
+pauseTimer.addEventListener('click', function () {
+  runTimer = false
+})
+
+resetTimer.addEventListener('click', function () {
+  runTimer = false
+  tLeft = 15 * 60
+  timer.textContent = `15:00`
+})
+
+// time left in seconds
+let tLeft = 60 * 15
+
+var downloadTimer = setInterval(function () {
+  if (runTimer == true && tLeft != 0) {
+    tLeft = tLeft - 1
+    time = `${Math.floor(tLeft / 60)}:`
+    if (tLeft % 60 < 10) {
+      time = time.concat(`0${tLeft % 60}`)
+      // this to handle when seconds is below 10 it will be like this 08 or 09 and so on
+    } else {
+      time = time.concat(`${tLeft % 60}`)
+    }
+    timer.textContent = time
+  }
+}, 1000)
+
+// ================Speed Code================================================================
+
+window.addEventListener('click', function () {
+  if (maxSpeedInput.value > 100) {
+    maxSpeedInput.value = 100
+  } else if (maxSpeedInput.value < 0) {
+    maxSpeedInput.value = 0
+  }
+
+  increaseSpeedSlider.value = maxSpeedInput.value
+})
+
+function myFunction(val) {
+  maxSpeedInput.value = val
+}
