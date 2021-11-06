7f8d06f8e394f148e1bb27504828a50f44dbb6e6
mostafa4adel <es-mostafaadel2023@alexu.edu.eg>
2021-11-06 19:42:01 +0200

    JsFiles
    Jsfile added "app.js" also all elements are added in js file

---
+++ GUI/JSFiles/app.js
@@ -0,0 +1,25 @@
+const motor_1 = document.getElementById('motor1')
+const motor_2 = document.getElementById('motor2')
+const motor_3 = document.getElementById('motor3')
+const motor_4 = document.getElementById('motor4')
+
+const verticalMotor_1 = document.getElementById('verticalMotor1')
+const verticalMotor_2 = document.getElementById('verticalMotor2')
+
+// values to edit are x2 and y2 in "joystickLine" and mapped to 78.5
+const joystickLine = document.getElementById('joystickLine')
+// values to edit are cv and cy in "joystickCircle" and mapped to 78.5
+const joystickCircle = document.getElementById('joystickCircle')
+
+const timer = document.getElementById('timer')
+
+const increaseSpeedBtn = document.getElementById('increaseSpeedBtn')
+const increaseSpeedSlider = document.getElementById('increaseSpeedSlider')
+
+const pidState = document.getElementById('pidState')
+const temprature = document.getElementById('temprature')
+const pressure = document.getElementById('pressure')
+const velocity = document.getElementById('velocity')
+
+const dcArmState = document.getElementById('dcArmState')
+const pneumaticArmState = document.getElementById('pnumaticArmState')
