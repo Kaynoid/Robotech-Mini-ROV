9ad19743c4d3554c56cdc94528bbc6cc1f0e4026
unknown <mhabiba2002@gmail.com>
2022-01-02 22:36:06 +0200

    arduino send/receive gui pi comm

--- GUI/js/udpsend.js
+++ GUI/js/udpsend.js
@@ -1,10 +1,20 @@
 // Create the Socket
-var arrayBuffer = 1;
+const arrayBuffer = new ArrayBuffer(4);
+const View = new Int32Array(arrayBuffer);
+View[0] = 20;
+var socketId = socketId
+var onReceive = function(packet){console.log(packet);};
+chrome.sockets.udp.onReceive.addListener(onReceive);
 chrome.sockets.udp.create({}, function(socketInfo) {
     // The socket is created, now we can send some data
-    var socketId = socketInfo.socketId;
-    chrome.sockets.udp.send(socketId, arrayBuffer,
-      '127.0.0.1', 0, function(sendInfo) {
-        console.log("sent " + sendInfo.bytesSent);
-    }); 
+    socketId = socketInfo.socketId;
+    console.log("created socket" + socketId);
+    chrome.sockets.udp.bind(socketId, "127.0.0.1", 8080, function(){
+    	
+    	chrome.sockets.udp.send(socketId, arrayBuffer,
+      '127.0.0.1', 5555, function(sendInfo) {
+        //console.log("sent " + sendInfo.bytesSent);
+    });    	
+    });
+    
   });
--- GUI/scriptudp.py
+++ GUI/scriptudp.py
@@ -1,18 +1,16 @@
 import socket
 
- 
+import serial
 
 localIP     = "127.0.0.1"
 
-localPort   = 20001
+localPort   = 5555
 
 bufferSize  = 1024
 
- 
+msgFromServer = "Hello UDP Client"
 
-msgFromServer       = "Hello UDP Client"
-
-bytesToSend         = str.encode(msgFromServer)
+bytesToSend = str.encode(msgFromServer)
 
 # Create a datagram socket
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -41,3 +39,26 @@ while(True):
     # Sending a reply to client
 
     UDPServerSocket.sendto(bytesToSend, address)
+
+
+if __name__ == '__main__':
+    ser = serial.Serial("COM10", 9600, timeout=1)
+    ser.flush()
+
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
+            ser.write(b"blue\n")
+    
+        bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+
+        message = bytesAddressPair[0]
+
+        address = bytesAddressPair[1]
+
+        clientMsg = "Message from Client:{}".format(message)
+        clientIP  = "Client IP Address:{}".format(address)
+    
+        print(clientMsg)
+        print(clientIP)
